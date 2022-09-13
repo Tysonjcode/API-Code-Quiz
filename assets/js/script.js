9c9fb066b83e49c454c26b7ca6793554e41630cc
Tyson Jackson <tyson@alittlerdesigner>
2022-09-13 22:54:13 +0800

    more html changes and the start of the java script

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Which element is not HTML semantic element?",
+        choices: ["article", "aside", "div", "section"],
+        answer: "div"
+    },
+    {
+        title: "The function and  var are known as?",
+        choices: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
+        answer: "Declaration statements"
+    },
+    {
+        title: "Which one of the following is the correct way for calling the JavaScript code?",
+        choices: ["Preprocessor", "Triggering Event", "RMI", "Function/Method"],
+        answer: "Function/Method"
+    },
+    {
+        title: "Which of the following tags is used for inserting the largest heading in HTML?",
+        choices: ["<h3>", "<h1>", "<h5>", "<h6>"],
+        answer: "<h1>"
+    },
+    {
+        title: "The HTML attribute used to define the inline styles is?",
+        choices: ["style", "styles", "class", "ID"],
+        answer: "style"
+    }
+];
+
